673f636a55b268c55e0286a229d540d58e32cb7f
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-03-02 14:08:33 +0100

    add button, Dialog and implement more buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet, RouterLink  } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,MatToolbarModule,MatSidenavModule,MatIconModule, RouterOutlet, RouterLink],
+  imports: [CommonModule,MatToolbarModule,MatSidenavModule,MatIconModule, RouterOutlet, RouterLink,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatIconModule,MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  constructor(private dialog: MatDialog) {}
+openDialog(){
+  this.dialog.open(DialogAddUserComponent)
 
+}
 }
