c931ff5cceb449bf02006031a4c1d08051fb1c66
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-03-02 16:04:39 +0100

    implement dialog address zip code and more

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,13 +4,16 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDialog} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule,MatIconModule,MatTooltipModule],
+  imports: [MatButtonModule,MatIconModule,MatTooltipModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
