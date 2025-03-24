d1afb309fbef59038b3a9037752d9ca2af3392b2
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-03-24 17:22:31 +0100

    display users form Database(firebase)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,24 +9,37 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import {MatCardModule} from '@angular/material/card';
-
+import { Firestore, FirestoreModule, collection, collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { inject, OnInit } from '@angular/core';
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule,MatIconModule,MatTooltipModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,MatCardModule],
+  imports: [MatButtonModule,MatIconModule,MatTooltipModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,MatCardModule,CommonModule,FirestoreModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+
+
+
+export class UserComponent implements OnInit {
+  private firestore: Firestore = inject(Firestore);
+  allUsers$!: Observable<any[]>;
+
   constructor(private dialog: MatDialog) {}
-openDialog(){
-  this.dialog.open(DialogAddUserComponent)
 
-}
-user=new User();
+  ngOnInit() {
+    const usersCollection = collection(this.firestore, 'users');
+    this.allUsers$ = collectionData(usersCollection); 
+  }
 
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 
+  user = new User();
 }
