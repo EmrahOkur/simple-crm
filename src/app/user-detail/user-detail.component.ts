76af69a448bb7e77365bf7c11b78a3a7b09b09d7
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-03-30 13:58:20 +0200

    add firebase data to SimpleCrm

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { Firestore, collection, addDoc, doc, docData } from '@angular/fire/firestore';
+import { inject } from '@angular/core';
+import { User } from '../../models/user.class';
+
+
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  private firestore: Firestore = inject(Firestore);
+userId='';
+user:User=new User();
+  constructor(private route: ActivatedRoute,){}
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap =>{
+      this.userId=paramMap.get('id') || '';
+      console.log('Got Id', this.userId);
+      this.getUser();
+    })
+  }
+
+  getUser() {
+    const userDocRef = doc(this.firestore, 'users', this.userId); 
+    docData(userDocRef).subscribe((userData: any) => {
+      this.user = new User(userData);
+      console.log('Retrieved User', userData)
+    });
+  }
 }
